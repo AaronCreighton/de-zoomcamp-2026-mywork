0ba4ba305baff7faaefde56396f1ab3f3bcd2710
Aaron Creighton <aaron.s.creighton@gmail.com>
2026-08-28 18:20:43 +1200

    dynamic url

--- 02-airflow-workflow-orchestration/dags/Ingest_data_local.py
+++ 02-airflow-workflow-orchestration/dags/Ingest_data_local.py
@@ -1,11 +1,11 @@
 
 import os
-from datetime import datetime
+From datetime import datetime
 
 from airflow import DAG
 
-from airflow.operators.bash import BashOperator
-from airflow.operators.python import PythonOperator 
+from airflow.providers.standard.operators.bash import BashOperator
+from airflow.providers.standard.operators.python import PythonOperator 
 
 
 AIRFLOW_HOME = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
@@ -13,19 +13,21 @@ AIRFLOW_HOME = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
 # Schedule_interval="0 6 2 * *" means the DAG will run at 6:00 AM on the 2nd day of every month.
 # to update the schedule interval, you can use the cron expression from: https://crontab.guru/
 local_workflow = DAG(
-    dag_id="data_ingest_local_dag",
+    dag_id="data_ingest_local",
+    start_date=datetime(2021, 1, 1),
     schedule="0 6 2 * *",
-    start_date=datetime(2023, 1, 1),
 )
 
-url = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2021-01.csv.gz"
+URL_PREFIX = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/"
+URL_TEMPLATE = URL_PREFIX + "/yellow_tripdata_{{ logical_date.strftime(\'%Y-%m\') }}.csv.gz"
+OUTPUT_FILE_TEMPLATE = AIRFLOW_HOME + "/yellow_tripdata_{{ logical_date.strftime(\'%Y-%m\') }}.csv.gz"
 
 
 with local_workflow:
     
     wget_task = BashOperator(
         task_id="wget_task",
-        bash_command=f'wget {url} -O {AIRFLOW_HOME}/yellow_tripdata_2021-01.csv.gz'
+        bash_command=f'wget {URL_TEMPLATE} -O {OUTPUT_FILE_TEMPLATE}'
     )
     
     ingest_task = BashOperator(
