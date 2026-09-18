29de14f3dc1b3d316d870420de41613327d1a3b2
Aaron Creighton <aaron.s.creighton@gmail.com>
2026-09-18 16:32:02 +1200

    Load to GCP

--- 02-airflow-workflow-orchestration/dags/08_gcp_taxi_dag.py
+++ 02-airflow-workflow-orchestration/dags/08_gcp_taxi_dag.py
@@ -5,18 +5,19 @@
 import os
 from datetime import datetime
 
+
 from airflow.sdk import dag, task
 
 from airflow.providers.standard.operators.bash import BashOperator
-from airflow.providers.standard.operators.python import PythonOperator
-from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
 
-
-from load_taxi_local import load_callable
 
 AIRFLOW_HOME = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
 CHUNKSIZE=100000
-PG_CONN_ID="pg_ny_taxi"
+
+
+# GCP variables
+GCP_CONN_ID = "gcp_zoomcamp"
 
 
 # Schedule_interval="0 6 2 * *" means the DAG will run at 6:00 AM on the 2nd day of every month.
@@ -28,11 +29,16 @@ DATASETS = {
 }
     
 def make_dag(TAXI_COLOUR, CONFIG):
-    URL_PREFIX = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/" + TAXI_COLOUR + "/"
-    URL_TEMPLATE = URL_PREFIX + "/" + TAXI_COLOUR + "_tripdata_{{ logical_date.strftime(\'%Y-%m\') }}.csv.gz"
-    OUTPUT_FILE_TEMPLATE = AIRFLOW_HOME + "/" + TAXI_COLOUR + "_tripdata_{{ logical_date.strftime(\'%Y-%m\') }}.csv.gz"
-    STAGING_TABLE='tripdata_'+ TAXI_COLOUR + '_staging'
-    FINAL_TABLE='tripdata_'+ TAXI_COLOUR
+    URL_PREFIX = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/" + TAXI_COLOUR + "/"   
+    COlOUR_DATE =  TAXI_COLOUR + "_tripdata_{{ logical_date.strftime(\'%Y-%m\') }}"
+    URL_TEMPLATE = URL_PREFIX + "/" + COlOUR_DATE + ".csv.gz"
+    OUTPUT_FILE_TEMPLATE = AIRFLOW_HOME + "/" + COlOUR_DATE + ".csv"
+    
+    # GCP variables
+    GCS_OBJECT_PREFIX = "taxi/raw/" + TAXI_COLOUR + "_tripdata/{{ logical_date.strftime(\'%Y/%m\') }}.csv"
+    
+    #STAGING_TABLE='tripdata_'+ TAXI_COLOUR + '_staging'
+    #FINAL_TABLE='tripdata_'+ TAXI_COLOUR
      
     # newer Taskflow API, of Airflow Dag factory function, compared to postgres dag.
     @dag(
@@ -44,42 +50,25 @@ def make_dag(TAXI_COLOUR, CONFIG):
 
         extract_task = BashOperator(
             task_id="extract_task",
-            bash_command=f'wget {URL_TEMPLATE} -O {OUTPUT_FILE_TEMPLATE}'
+            bash_command="wget -qO- " + URL_TEMPLATE + " | gunzip > " + OUTPUT_FILE_TEMPLATE
         )
         
-        @task(task_id="load_task")
-        def load_task(URL_TEMPLATE, PG_CONN_ID, TAXI_COLOUR, STAGING_TABLE, CHUNKSIZE):
-                load_callable(
-                    url=URL_TEMPLATE, 
-                    pg_conn_id=PG_CONN_ID,
-                    taxi_colour=TAXI_COLOUR,
-                    target_table=STAGING_TABLE,
-                    chunksize=CHUNKSIZE,
-                )
-
-
-        transform_task = SQLExecuteQueryOperator(
-            task_id="transform_task",
-            conn_id=PG_CONN_ID,
-            sql=f"sql/transform_"+TAXI_COLOUR+".sql",
-            params={"final_table": FINAL_TABLE,
-                    "staging_table": STAGING_TABLE,
-                    "taxi_colour": TAXI_COLOUR
-                    },
-            split_statements=True,
-            autocommit=False, #make true for testing, making each statement a transaction. 
+        upload_task = LocalFilesystemToGCSOperator(
+            task_id="upload_to_gcs",
+            gcp_conn_id=GCP_CONN_ID,
+            src=OUTPUT_FILE_TEMPLATE,
+            dst=GCS_OBJECT_PREFIX,
+            bucket="{{var.value.GCP_BUCKET}}",
         )
         
-        cleanup = BashOperator(
-            task_id="cleanup",
-            bash_command="rm -f " + OUTPUT_FILE_TEMPLATE,
+        #cleanup = BashOperator(
+            #task_id="cleanup",
+            #bash_command="rm -f " + OUTPUT_FILE_TEMPLATE,
             # trigger_rule="all_done",  # enable once failures are handled elsewhere —
             # skips cleanup on failure by default, which keeps the file for inspection
-        )
+        #)
         
-        #loaded = load_task(URL_TEMPLATE, PG_CONN_ID, TAXI_COLOUR, STAGING_TABLE, CHUNKSIZE)
-        extract_task >> load_task(URL_TEMPLATE, PG_CONN_ID, TAXI_COLOUR, STAGING_TABLE, CHUNKSIZE) >> transform_task >> cleanup
-        #extract_task >> loaded >> transform_task >> cleanup
+        extract_task >> upload_task #>> cleanup
 
     return local_workflow()
 
