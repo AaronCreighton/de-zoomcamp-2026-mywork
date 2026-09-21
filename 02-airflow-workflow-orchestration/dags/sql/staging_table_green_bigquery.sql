15172c5fdd0d053e24406ab5227d1a7682c40980
Aaron Creighton <aaron.s.creighton@gmail.com>
2026-09-21 16:24:48 +1200

    match with postgres pipeline

--- 02-airflow-workflow-orchestration/dags/sql/staging_table_green_bigquery.sql
+++ 02-airflow-workflow-orchestration/dags/sql/staging_table_green_bigquery.sql
@@ -6,7 +6,9 @@ CREATE OR REPLACE TABLE `{{params.project}}.{{ params.dataset }}.{{params.table}
               COALESCE(CAST(lpep_pickup_datetime AS STRING), ""),
               COALESCE(CAST(lpep_dropoff_datetime AS STRING), ""),
               COALESCE(CAST(PULocationID AS STRING), ""),
-              COALESCE(CAST(DOLocationID AS STRING), "")
+              COALESCE(CAST(DOLocationID AS STRING), ""),
+              COALESCE(CAST(fare_amount AS STRING), ""),
+              COALESCE(CAST(trip_distance AS STRING), "")
             )) AS unique_row_id,
             '{{params.filename}}_{{ logical_date.strftime("%Y-%m") }}.csv' AS filename,
             *
